a992062d3fca9fb05d69d48813ed5e3819a84ac6
Michael Cortez-Mejia <bayarea6fifty@gmail.com>
2022-05-11 18:56:04 -0700

    reorganized thought-controller

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -70,22 +70,6 @@ const thoughtController = {
             .catch(err => res.status(400).json(err));
     },
     
-    addReaction({ params, body }, res) {
-        Thought.findOneAndUpdate(
-            { _id: params.thoughtId },
-            { $push: { reactions: body }},
-            { new: true, runValidators: true }
-        )
-            .then(dbThoughtData => {
-                if (!dbThoughtData) {
-                    res.status(404).json({ message: 'No user found with this id!'});
-                    return;
-                }
-                res.json(dbThoughtData);
-            })
-            .catch(err => res.json(err));
-    },
-
     deleteThought({ params }, res) {
         Thought.findOneAndDelete({ _id: params.thoughtId})
             .then(deletedThought => {
@@ -109,6 +93,23 @@ const thoughtController = {
             .catch(err => res.json(err));
 
     },
+
+    addReaction({ params, body }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $push: { reactions: body }},
+            { new: true, runValidators: true }
+        )
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No user found with this id!'});
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => res.json(err));
+    },
+
 
     deleteReaction({ params }, res) {
         Thought.findOneAndUpdate(
