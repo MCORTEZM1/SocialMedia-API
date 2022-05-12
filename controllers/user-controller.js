4ec365dad8ffabfbfeeb509abed4f0e9c1afb2eb
Michael Cortez-Mejia <bayarea6fifty@gmail.com>
2022-05-11 18:38:08 -0700

    completed required user methods for schema: get all users, get a user, post a new user, put for userId, delete user, add friend, delete friend

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -57,6 +57,7 @@ const userController = {
     deleteUser({ params }, res) {
         User.findOneAndDelete({ _id: params.id })
             .then(dbUserData => {
+                console.log('This is dbUserData on DELETE: ', dbUserData);
                 if (!dbUserData) {
                     res.status(404).json({ message: 'No user found with this id!'});
                     return;
@@ -64,7 +65,37 @@ const userController = {
                 res.json(dbUserData);
             })
             .catch(err => res.json(err));
-    }
+    },
+
+    addFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId},
+            { $push: { friends: params.friendId }},
+            { new: true }
+        )
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id!'});
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+
+
+    }, 
+
+    deleteFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId},
+            { $pull: { friends: { friendId: params.friendId }}},
+            { new: true}
+        )
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.json(err));
+    },
+
+
 };
 
 module.exports = userController; 
